c4f52bb2539d7542ecef9464c6593e71680a2b0c
Yonghwi Kim <yh0921k@gmail.com>
2022-08-29 10:19:54 +0900

    init: 프로젝트 초기화
    - .gitignore, README.md 등 초기화

---
+++ src/main/java/study/spring_core_basic/InflearnSpringCoreBasicApplication.java
@@ -0,0 +1,12 @@
+package study.spring_core_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCoreBasicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InflearnSpringCoreBasicApplication.class, args);
+  }
+}
---
+++ src/test/java/study/spring_core_basic/InflearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_core_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
