e483bc82be58615a3596249c60339d460a893d4f
Yonghwi Kim <yh0921k@gmail.com>
2022-09-04 21:03:04 +0900

    add: Section 03. AppConfig Refactoring 완료

--- src/main/java/study/spring_core_basic/AppConfig.java
+++ src/main/java/study/spring_core_basic/AppConfig.java
@@ -1,6 +1,8 @@
 package study.spring_core_basic;
 
+import study.spring_core_basic.discount.DiscountPolicy;
 import study.spring_core_basic.discount.FixDiscountPolicy;
+import study.spring_core_basic.member.MemberRepository;
 import study.spring_core_basic.member.MemberService;
 import study.spring_core_basic.member.MemberServiceImpl;
 import study.spring_core_basic.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import study.spring_core_basic.order.OrderServiceImpl;
 public class AppConfig {
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
+  }
+
+  private MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  public DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 }
