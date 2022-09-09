5c446dcb7f821208f0919f4a1280fabcc4a7a204
Yonghwi Kim <yh0921k@gmail.com>
2022-09-09 11:41:17 +0900

    add: Section 04. 스프링 빈 조회 기본 완료

---
+++ src/test/java/study/spring_core_basic/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,44 @@
+package study.spring_core_basic.beanfind;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import study.spring_core_basic.AppConfig;
+import study.spring_core_basic.member.MemberService;
+import study.spring_core_basic.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+  AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+  @Test
+  @DisplayName("빈 이름으로 조회")
+  void findBeanByName() {
+    Object memberService = ac.getBean("memberService", MemberService.class);
+    assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+  }
+
+  @Test
+  @DisplayName("이름 없이 타입으로만 조회")
+  void findBeanByType() {
+    Object memberService = ac.getBean(MemberService.class);
+    assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+  }
+
+  @Test
+  @DisplayName("구체 타입으로 조회")
+  void findBeanByNameAndConcreteType() {
+    Object memberService = ac.getBean("memberService", MemberServiceImpl.class);
+    assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+  }
+
+  @Test
+  @DisplayName("빈 이름으로 조회되는 경우가 없는 경우")
+  void findBeanException() {
+    assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("UndefinedBean", MemberService.class));
+  }
+
+}
