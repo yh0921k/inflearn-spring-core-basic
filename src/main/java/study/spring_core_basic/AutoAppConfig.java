2a4a4cbe167aa82b2b37b3855d2195b961343e7d
Yonghwi Kim <yh0921k@gmail.com>
2022-09-18 02:50:07 +0900

    add: Section 06. 컴포넌트 스캔 - 중복 등록과 충돌 완료

--- src/main/java/study/spring_core_basic/AutoAppConfig.java
+++ src/main/java/study/spring_core_basic/AutoAppConfig.java
@@ -1,11 +1,20 @@
 package study.spring_core_basic;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import study.spring_core_basic.member.MemberRepository;
+import study.spring_core_basic.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
     excludeFilters =
         @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
-public class AutoAppConfig {}
+public class AutoAppConfig {
+
+  @Bean(name = "memoryMemberRepository")
+  public MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+}
