e30de22f6da376c95473cf9a5878cadb4446416f
Yonghwi Kim <yh0921k@gmail.com>
2022-10-18 14:24:49 +0900

    add: Section 09. 빈 스코프 - 스코프와 프록시 완료

--- src/main/java/study/spring_core_basic/web/LogDemoService.java
+++ src/main/java/study/spring_core_basic/web/LogDemoService.java
@@ -1,7 +1,6 @@
 package study.spring_core_basic.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import study.spring_core_basic.common.MyLogger;
 
@@ -9,10 +8,9 @@ import study.spring_core_basic.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-  private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+  private final MyLogger myLogger;
 
   public void logic(String id) {
-    MyLogger myLogger = myLoggerObjectProvider.getObject();
     myLogger.log("service id = " + id);
   }
 }
